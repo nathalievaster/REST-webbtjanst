edd0efd5d4fbc34385366c42f09554806924caf5
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-04-18 14:33:02 +0200

    Testar min thunderclient

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();
 
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/api", (req, res) => {
+    res.json({message: "Welcome to my api!"});
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 })
